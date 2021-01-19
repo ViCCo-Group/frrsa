ec82ad2a8abda06cb6bdcd1808e73d40c6e3938e
Philipp Kaniuth <kaniuth@cbs.mg.de>
2021-01-19 15:30:42 +0100

    first commit

---
+++ frrsa/test.py
@@ -0,0 +1,35 @@
+from numpy.random import default_rng
+from fitting.crossvalidation import tune_rsa
+
+# Specify a seed for reproducible results.
+rng = default_rng(seed=4)
+
+n_units = 1000 # How many measurement channels?
+n_objects = 40 # How many objects aka conditions?
+n_outputs = 2 # How many different outputs?
+
+# Simulate output and inputs.
+output = rng.integers(low=0, high=100, size=(n_objects,n_objects,n_outputs))
+inputs = rng.integers(low=0, high=100, size=(n_units,n_objects))
+
+
+
+#%% Call the main funtion.
+outer_k = 2
+outer_reps = 2
+splitter = 'random'
+hyperparams = None
+score_type = 'pearsonr'
+
+predicted_RDM, predictions, unfitted_scores, crossval, betas, fitted_scores = tune_rsa(output,
+                                                                                       inputs, 
+                                                                                       outer_k, 
+                                                                                       outer_reps, 
+                                                                                       splitter, 
+                                                                                       hyperparams, 
+                                                                                       score_type, 
+                                                                                       sanity=False, 
+                                                                                       rng_state=None)
+
+
+#%% End of script
