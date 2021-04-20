0faa319b1af2961a029d1cc26319f43f70355a43
Philipp Kaniuth <kaniuth@cbs.mg.de>
2021-04-20 13:31:14 +0200

    got rid of testing loop

--- frrsa/test.py
+++ frrsa/test.py
@@ -10,6 +10,7 @@ n_outputs = 2 # How many different outputs?
 
 # Simulate output and inputs.
 output = rng.integers(low=0, high=100, size=(n_objects,n_objects,n_outputs))
+inputs = rng.integers(low=0, high=100, size=(n_units,n_objects))
 
 
 #%% Call the main funtion.
@@ -18,36 +19,20 @@ outer_reps = 3
 splitter = 'random'
 hyperparams = None
 score_type = 'pearson'
+rng_state = None
+sanity = False
+parallel = True
 
-import time
-
-inputs = rng.integers(low=0, high=100, size=(n_units,n_objects))
-
-for parallel in [True, False]:
-    s = time.time()
-    predicted_RDM, predictions, unfitted_scores, crossval, betas, fitted_scores = frrsa(output,
-                                                                                            inputs, 
-                                                                                            outer_k, 
-                                                                                            outer_reps, 
-                                                                                            splitter, 
-                                                                                            hyperparams, 
-                                                                                            score_type, 
-                                                                                            sanity=False, 
-                                                                                            rng_state=1,
-                                                                                            parallel=parallel)
-
-    result = {
-        'rdm': predicted_RDM,
-        'predictions': predictions,
-        'unfitted_scores': unfitted_scores,
-        'crossval': crossval,
-        'betas': betas,
-        'fitted_scores': fitted_scores
-    }
-
-    import pickle
-    with open('result%s.pickle' % parallel, 'wb') as handle:
-        pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
+predicted_RDM, predictions, unfitted_scores, crossval, betas, fitted_scores = frrsa(output,
+                                                                                        inputs, 
+                                                                                        outer_k, 
+                                                                                        outer_reps, 
+                                                                                        splitter, 
+                                                                                        hyperparams, 
+                                                                                        score_type, 
+                                                                                        sanity=sanity, 
+                                                                                        rng_state=rng_state,
+                                                                                        parallel=parallel)
 
 
 #%% End of script
