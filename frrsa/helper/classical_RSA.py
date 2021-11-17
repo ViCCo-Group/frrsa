9255f365e58689600f31025c21230e6f8798b1d5
Philipp Kaniuth <kaniuth@cbs.mg.de>
2021-11-17 15:08:25 +0100

    deprecated func "noise_ceiling"

--- frrsa/helper/classical_RSA.py
+++ frrsa/helper/classical_RSA.py
@@ -8,7 +8,7 @@ Contains function to perform classical Representational Similarity Analysis.
 """
 
 import numpy as np
-from scipy.stats import spearmanr, pearsonr, rankdata
+from scipy.stats import spearmanr, pearsonr
 from functools import partial
 from scipy.spatial.distance import squareform
 
@@ -84,58 +84,3 @@ def correlate_RDMs(rdv1, rdv2, score_type='pearson'):
     elif score_type == 'spearman':
         corr, p_value = spearmanr(rdv1, rdv2)
     return corr, p_value
-
-
-def noise_ceiling(reference_rdms, correlation='pearson'):
-    '''Compute noise ceilings for represntational similarity analysis.
-    
-    Parameters
-    ----------
-    reference_rdms : ndarray
-        Several dissimilarity matrices based on which the noise ceilings shall
-        be computed. The shape (n,n,m) is mandatory, where m denotes
-        different matrices of shape (n,n).
-    correlation : {'pearson', 'spearman'}, optional
-        The correlation coefficient which should be used to compute ceilings
-        (defaults to `pearson`).
-        
-    Returns
-    -------
-        ceilings : ndarray
-            Upper and lower noise ceiling value.
-        
-    Notes
-    -----
-    This implementation is inspired by the MATLAB implementation presented in [1]_.
-    
-    References
-    ----------
-    .. [1] Nili, H., Wingfield, C., Walther, A., Su, L., Marslen-Wilson, W.,
-       & Kriegeskorte, N. (2014). A Toolbox for Representational Similarity
-       Analysis. PLoS Computational Biology, 10(4), e1003553.
-       https://doi.org/10.1371/journal.pcbi.1003553
-    '''
-    n_subjects = reference_rdms.shape[2]
-    reference_rdms = flatten_RDM(reference_rdms)
-    if correlation=='pearson':
-        reference_rdms = (reference_rdms - reference_rdms.mean(0)) / reference_rdms.std(0)
-    elif correlation=='spearman':
-        reference_rdms = rankdata(reference_rdms, axis=0)
-    #TODO: maybe implement Kendall's tau_a
-    
-    reference_rdm_average = np.mean(reference_rdms, axis=1)
-    upper_bound = 0
-    lower_bound = 0
-    
-    for n in range(n_subjects):
-        index = list(range(n_subjects))
-        index.remove(n)
-        rdm_n = reference_rdms[:,n]
-        reference_rdm_average_loo = np.mean(reference_rdms[:,index], axis=1)
-        upper_bound += np.corrcoef(reference_rdm_average, rdm_n)[0][1]
-        lower_bound += np.corrcoef(reference_rdm_average_loo, rdm_n)[0][1]
-        #TODO: maybe implement Kendall's tau_a
-        
-    upper_bound /= n_subjects
-    lower_bound /= n_subjects
-    return np.array([upper_bound, lower_bound])
