12d73b8e545af905a00a190bb930c06fb1bac760
Philipp Kaniuth <kaniuth@cbs.mg.de>
2022-01-07 17:58:35 +0100

    added test for classical_rsa.make_RDM when distance="sqeuclidean"

--- frrsa/tests/test_classical_rsa.py
+++ frrsa/tests/test_classical_rsa.py
@@ -10,8 +10,16 @@ class TestMakeRDM(unittest.TestCase):
         activity_pattern_matrix = np.array([[0.5, 0.8], 
                                             [0.6, 0.9]])
         rdm = make_RDM(activity_pattern_matrix, distance='pearson')
-        expected_rdm = np.array([[0.,0.], 
-                                 [0.,0.]])
+        expected_rdm = np.array([[0., 0.], 
+                                 [0., 0.]])
+        assert_allclose(rdm, expected_rdm)
+        
+    def test_sqeuclidean(self):
+        activity_pattern_matrix = np.array([[1, 4], 
+                                            [2, 1]])
+        rdm = make_RDM(activity_pattern_matrix, distance='sqeuclidean')
+        expected_rdm = np.array([[0., 10.], 
+                                 [10., 0.]])
         assert_allclose(rdm, expected_rdm)
 
 class TestFlattenRDM(unittest.TestCase):
