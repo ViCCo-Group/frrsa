f5390c64341e62e8f60716aad188a7f4386c30f2
Philipp Kaniuth <kaniuth@cbs.mg.de>
2022-04-22 18:24:38 +0200

    adpated tests to recent changes

--- frrsa/tests/test_classical_rsa.py
+++ frrsa/tests/test_classical_rsa.py
@@ -1,53 +1,54 @@
 import unittest
 
-from frrsa.helper.classical_RSA import make_RDM, flatten_RDM
+from frrsa.helper.classical_RSA import make_RDM, flatten_matrix
 
 import numpy as np
 from numpy.testing import assert_allclose
 
+
 class TestMakeRDM(unittest.TestCase):
     def test_pearson(self):
-        activity_pattern_matrix = np.array([[0.5, 0.8], 
+        activity_pattern_matrix = np.array([[0.5, 0.8],
                                             [0.6, 0.9]])
         rdm = make_RDM(activity_pattern_matrix, distance='pearson')
-        expected_rdm = np.array([[0., 0.], 
+        expected_rdm = np.array([[0., 0.],
                                  [0., 0.]])
         assert_allclose(rdm, expected_rdm)
-        
+
     def test_sqeuclidean(self):
-        activity_pattern_matrix = np.array([[1, 4], 
+        activity_pattern_matrix = np.array([[1, 4],
                                             [2, 1]])
         rdm = make_RDM(activity_pattern_matrix, distance='sqeuclidean')
-        expected_rdm = np.array([[0., 10.], 
+        expected_rdm = np.array([[0., 10.],
                                  [10., 0.]])
         assert_allclose(rdm, expected_rdm)
 
-class TestFlattenRDM(unittest.TestCase):
-    def test_flatten_single_rdm(self):
-        rdm = np.array([[1, 0.5, 0.6], 
-                        [0.5, 1, 0.7], 
-                        [0.6, 0.7, 1]])
-        rdv = flatten_RDM(rdm)
-        expected_rdv = np.array([[0.5],
-                                 [0.6],
-                                 [0.7]])
-        assert_allclose(expected_rdv, rdv)
 
-    def test_flatten_multiple_rdm(self):
-        rdms = np.array([[[1, 2],
-                          [0.4, 0.7],
-                          [0.5, 0.8]],
+class TestFlattenMatrix(unittest.TestCase):
+    def test_flatten_single_matrix(self):
+        representational_matrix = np.array([[1, 0.5, 0.6],
+                                            [0.5, 1, 0.7],
+                                            [0.6, 0.7, 1]])
+        representational_vector = flatten_matrix(representational_matrix)
+        expected_vector = np.array([[0.5],
+                                    [0.6],
+                                    [0.7]])
+        assert_allclose(expected_vector, representational_vector)
 
-                         [[0.4, 0.7],
-                          [1, 2],
-                          [0.6, 0.9]],
+    def test_flatten_multiple_matrices(self):
+        representational_matrices = np.array([[[1, 2],
+                                              [0.4, 0.7],
+                                              [0.5, 0.8]],
 
-                         [[0.5,  0.8],
-                          [0.6, 0.9],
-                          [1, 2]]])
-        rdv = flatten_RDM(rdms)
-        expected_rdv = np.array([[0.4, 0.7],
-                                 [0.5, 0.8],
-                                 [0.6, 0.9]])
-        assert_allclose(expected_rdv, rdv)
+                                             [[0.4, 0.7],
+                                              [1, 2],
+                                              [0.6, 0.9]],
 
+                                             [[0.5, 0.8],
+                                              [0.6, 0.9],
+                                              [1, 2]]])
+        representational_vectors = flatten_matrix(representational_matrices)
+        expected_vectors = np.array([[0.4, 0.7],
+                                     [0.5, 0.8],
+                                     [0.6, 0.9]])
+        assert_allclose(expected_vectors, representational_vectors)
